09d400f0df0b31b48ff1e2536f1fb701d3dc51cf
Arshia Keshvari <110261434+TeslaNeuro@users.noreply.github.com>
2025-06-03 10:34:20 +0100

    Update phil_collins.py

--- phil_collins.py
+++ phil_collins.py
@@ -1,22 +1,64 @@
 import streamlit as st
+import random
+import time
 
 st.set_page_config(page_title="Rat Race + Bass Boosted Vibes", layout="wide")
 
-st.title("🐭 Rat Race with Bass Boosted Vibes (YouTube Video)")
+st.markdown(
+    """
+    <style>
+    .bass-boosted {
+        animation: shake 0.5s;
+        animation-iteration-count: infinite;
+    }
 
-# Embed the YouTube video
-youtube_url = "https://www.youtube.com/embed/i3XcYb2ZTO8?autoplay=0&mute=0"
-st.video(youtube_url)
+    @keyframes shake {
+      0% { transform: translate(1px, 1px) rotate(0deg); }
+      10% { transform: translate(-1px, -2px) rotate(-1deg); }
+      20% { transform: translate(-3px, 0px) rotate(1deg); }
+      30% { transform: translate(3px, 2px) rotate(0deg); }
+      40% { transform: translate(1px, -1px) rotate(1deg); }
+      50% { transform: translate(-1px, 2px) rotate(-1deg); }
+      60% { transform: translate(-3px, 1px) rotate(0deg); }
+      70% { transform: translate(3px, 1px) rotate(-1deg); }
+      80% { transform: translate(-1px, -1px) rotate(1deg); }
+      90% { transform: translate(1px, 2px) rotate(0deg); }
+      100% { transform: translate(1px, -2px) rotate(-1deg); }
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
 
+st.title("🐭 Rat Race with Bass Boosted Vibes")
+
+# Embedded YouTube video with optional bass boost effect
+with st.container():
+    bass_level = st.slider("🎚️ Bass Level", 0, 10, 5)
+    bass_class = "bass-boosted" if bass_level >= 8 else ""
+    st.markdown(
+        f'<div class="{bass_class}"><iframe width="100%" height="400" src="https://www.youtube.com/embed/i3XcYb2ZTO8?autoplay=0&mute=0" frameborder="0" allowfullscreen></iframe></div>',
+        unsafe_allow_html=True,
+    )
+
+# Button to simulate "boost" and show random rat race GIFs
 st.subheader("🐀 Live Rat Race GIFs")
 
 rat_race_gifs = [
-    "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExbHZteXUyNWllcnF3OG45dGdjb3Mzc3lyOWRsczJvdzU4YnE1MzhqdiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/nO5yKct2k5xCM/giphy.gif",
-    "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExcnV6bHJpbm1lOGk4N2t1d2tmMDRjc25mbHozeXJtaDJ3ZDhnaDA5OCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/CBdAAvsd63x5u/giphy.gif",
-    "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExMmhxa2F4bnc3bW0wN2l2bWFsMXhxOTNjam0xaDdycG42b29ueWpxciZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/n3buvz9Fa62Ri/giphy.gif",
+    "https://media1.giphy.com/media/nO5yKct2k5xCM/giphy.gif",
+    "https://media3.giphy.com/media/CBdAAvsd63x5u/giphy.gif",
+    "https://media4.giphy.com/media/n3buvz9Fa62Ri/giphy.gif",
+    "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExOGY5NmltNnRyNm51bWx4YjVqeW4yZnF4ZXcyNmFpeTc5eDZwdGU5cSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/N0nyPOrKptLMzzi6ym/giphy.gif",
+    "https://media4.giphy.com/media/3o7abldj0b3rxrZUxW/giphy.gif",
 ]
 
-cols = st.columns(len(rat_race_gifs))
-for i, gif_url in enumerate(rat_race_gifs):
+if st.button("💣 DROP THE BASS"):
+    st.toast("🔥 Bass boosted! Rats going wild!")
+    gifs_to_show = random.sample(rat_race_gifs, 3)
+else:
+    gifs_to_show = rat_race_gifs[:3]
+
+cols = st.columns(len(gifs_to_show))
+for i, gif_url in enumerate(gifs_to_show):
     with cols[i]:
         st.image(gif_url, use_container_width=True)
